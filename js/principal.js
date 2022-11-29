3bfe989874f171a3f844353a2f071c684aca5e68
jimennarocha <jimenna.rocha@gmail.com>
2022-11-29 14:30:50 -0300

    estilos com js

--- js/principal.js
+++ js/principal.js
@@ -21,11 +21,13 @@ for (var i = 0; i < pacientes.length; i++) {
     if (peso <= 0 || peso >= 1000) {
         pesoEhValido = false;
         tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-inválido");
     }
 
     if (altura <= 0 || altura >= 3.00) {
         alturaEhValida = false;
         tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-inválido");
     }
 
   if (alturaEhValida && pesoEhValido) {
